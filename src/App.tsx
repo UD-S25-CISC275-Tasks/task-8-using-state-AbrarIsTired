208bc461a87374209fe87a2381ec4b2903c23133
AbrarIsTired <abraramn@udel.edu>
2025-03-07 11:39:58 -0500

    Added more Documentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import "./App.css";
-
-function App(): React.JSX.Element {
-=======
 import React, { JSX } from "react";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
@@ -14,18 +8,11 @@ import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
 
 function App(): JSX.Element {
->>>>>>> solved-state
     return (
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-<<<<<<< HEAD
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -38,7 +25,6 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> solved-state
         </div>
     );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -1,28 +1,35 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+//Random Number Generator between 1 and 6 Inclusive
 export function d6(): number {
     return 1 + Math.floor(Math.random() * 6);
 }
 
 export function TwoDice(): React.JSX.Element {
-    const [leftDie, setLeftDie] = useState<number>(() => d6());
+    const [leftDie, setLeftDie] = useState<number>(() => d6()); //Setting the States: Left. Uses D6 to set a random value
+
     const [rightDie, setRightDie] = useState<number>(() => {
+        //Setting the States: Right. Sets the D6 Random to != the D6 Left
         let initial = d6();
         while (initial === leftDie) {
+            //If D6 == Left D6 then reroll
             initial = d6();
         }
         return initial;
     });
 
+    //Rolls the dice and updates the State
     const rollLeftDie = () => {
         setLeftDie(d6());
     };
 
+    //Rolls the dice and updates the State
     const rollRightDie = () => {
         setRightDie(d6());
     };
 
+    //Comparing Scores
     let gameMessage = "";
     if (leftDie === rightDie) {
         if (leftDie === 1) {
